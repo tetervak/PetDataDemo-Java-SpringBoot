9b5daff149bff1b20301d4d4b8c73ec7bd9c03f9
Alex Tetervak <tetervak@sheridancollege.ca>
2023-05-06 10:46:53 -0400

    added request log filter

---
+++ src/main/java/ca/tetervak/petdatademo/filter/RequestLogFilter.java
@@ -0,0 +1,26 @@
+package ca.tetervak.petdatademo.filter;
+
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class RequestLogFilter implements Filter {
+
+    private final Logger log = LoggerFactory.getLogger(RequestLogFilter.class);
+
+    @Override
+    public void doFilter(ServletRequest servletRequest,
+                         ServletResponse servletResponse,
+                         FilterChain filterChain) throws IOException, ServletException {
+        String requestUri = ((HttpServletRequest)servletRequest).getRequestURI();
+
+        log.info("received request for " + requestUri);
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+}
